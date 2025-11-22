7dcb582277e6f31fbf85a07e95d3ea57c87b95e4
Shree1417 <poornashreesankaran14@gmail.com>
2025-11-22 22:07:32 +0530

    added

---
+++ SQL/update/views.sql
@@ -0,0 +1,38 @@
+USE SLA;
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+DESC employees;
+INSERT INTO employees (emp_id, name, department, salary) VALUES
+(1, 'Arsha', 'HR', 30000),
+(2, 'Banu', 'Sales', 35000),
+(3, 'Chakra', 'HR', 40000),
+(4, 'Dharshu', 'IT', 60000),
+(5, 'Emily', 'IT', 55000);
+
+CREATE VIEW hr_employees AS
+SELECT emp_id, name, salary FROM employees WHERE department = 'HR';
+
+SELECT * FROM hr_employees;
+
+CREATE VIEW employee_salary_info AS
+SELECT name,department,salary,salary * 12 AS annual_salary FROM employees;
+
+SELECT * FROM employee_salary_info;
+
+CREATE VIEW it_employees AS
+SELECT emp_id, name, salary FROM employees WHERE department = 'IT';
+
+UPDATE it_employees SET salary = 65000 WHERE emp_id = 4;
+
+SELECT * FROM employees WHERE emp_id = 4;
+
+INSERT INTO it_employees (emp_id, name, salary)
+VALUES (6, 'Fathima', 50000);
+
+DELETE FROM it_employees WHERE emp_id = 5;
+
+SELECT * FROM employees;
