fa4eb9c79878ed856178850b1e8698614d6eaccf
Shree1417 <poornashreesankaran14@gmail.com>
2025-11-29 09:33:34 +0530

    added

--- SQL/update/stringfunction.sql
+++ SQL/update/stringfunction.sql
@@ -10,11 +10,11 @@ CREATE TABLE orders (
 );
 
 INSERT INTO orders (product_name, quantity, price, event_name, event_date) VALUES
-(1, 101, 'Apple Watch', 2, 350.00, '2025-01-10'),
-(2, 102, 'AirPods', 1, 150.00, '2025-01-11'),
-(3, 103, 'Asus Laptop', 3, 750.00, '2025-01-12'),
-(4, 104, 'Bluetooth Speaker', 4, 120.00, '2025-01-13'),
-(5, 105, 'Android Tablet', 5, 280.00, '2025-01-14');
+('Apple Watch', 2, 350.00, 'Apple Watch Launch', '2025-01-10'),
+('AirPods', 1, 150.00, 'AirPods Showcase', '2025-01-11'),
+('Asus Laptop', 3, 750.00, 'Asus Tech Conference', '2025-01-12'),
+('Bluetooth Speaker', 4, 120.00, 'Bluetooth Speaker Expo', '2025-01-13'),
+('Android Tablet', 5, 280.00, 'Android Tablet Summit', '2025-01-14');
 
 
 SELECT *FROM orders WHERE quantity > 2;
